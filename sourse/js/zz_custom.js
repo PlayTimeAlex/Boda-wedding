71554a015de62c89b1d61fe41dfa6f722e60d460
PlayTimeAlex <alex619@bigmir.net>
2015-06-14 21:36:13 +0300

    form

--- sourse/js/zz_custom.js
+++ sourse/js/zz_custom.js
@@ -60,6 +60,76 @@
                 });
             });
         }
+
+        $('.b-header__mobmenubtn').on('click', function(){
+            var $menu = $('.b-tnav');
+            if($menu.hasClass('open')){
+                $menu.slideUp(150, function(){
+                    $menu.removeClass('open');
+                });
+                return false;
+            }
+            $menu.slideDown(150, function(){
+                $menu.addClass('open');
+            });
+            return false;
+        });
+
+        $( ".b-form__datepicker" ).datepicker();
+        $(".b-form__slider").each(function () {
+            var max = $(this).data("max");
+            var min = $(this).data("min");
+            var el = $(this).data("el");
+            $(this).slider({
+                min: min,
+                max: max,
+                slide: function( event, ui ) {
+                    console.log(ui)
+                    $('.'+el).html(ui.value);
+                    $('#'+el).val(ui.value);
+                }
+            });
+        });
+
+        /* Russian (UTF-8) initialisation for the jQuery UI date picker plugin. */
+        /* Written by Andrew Stromnov (stromnov@gmail.com). */
+        (function( factory ) {
+            if ( typeof define === "function" && define.amd ) {
+
+                // AMD. Register as an anonymous module.
+                define([ "../datepicker" ], factory );
+            } else {
+
+                // Browser globals
+                factory( jQuery.datepicker );
+            }
+        }(function( datepicker ) {
+
+            datepicker.regional['ru'] = {
+                closeText: 'Закрыть',
+                prevText: '&#x3C;Пред',
+                nextText: 'След&#x3E;',
+                currentText: 'Сегодня',
+                monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+                    'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+                monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+                    'Июл','Авг','Сен','Окт','Ноя','Дек'],
+                dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+                dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+                dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+                weekHeader: 'Нед',
+                dateFormat: 'dd.mm.yy',
+                firstDay: 1,
+                isRTL: false,
+                showMonthAfterYear: false,
+                yearSuffix: ''};
+            datepicker.setDefaults(datepicker.regional['ru']);
+
+            return datepicker.regional['ru'];
+
+        }));
+
+        $('.b-form__select').styler();
     });
 
     $(window).load(function() {
